2aefda1b0515ae816c3c4150064cd2b40db3b3da
therealmvelo <2020130474@students,elangeni.edu.za>
2024-07-17 16:57:34 +0200

    jh

--- index.php
+++ index.php
@@ -1,4 +1,47 @@
-<?php
-echo "hello world";
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="utf-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1">
+       <link rel="stylesheet" href="index.css">
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+       <title>workout</title>
+   </head>
+   <body>
+        <nav class="navbar navbar-expand-lg border border-success">
+            <a href="#title" class="navbar-brand m-lg-2">Hello world</a>
+            <ul class="navbar-nav justify-items-center m-auto d-flex">
+                <li class="nav-link">Home</li>
+                <li class="nav-link">About</li>
+                <li class="nav-link">Contact</li>
+                <li class="nav-link">Read more</li>
+            </ul>
+        </nav>
+        <div class="container">
+            <div class="body-content">
+            <div id="imgsCarousel" class="carousel slide">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img src="imgs/workout0.avif" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="imgs/workout0.avif" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                    <img src="imgs/workout0.avif" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+                </div>
+            </div>
+        </div>
+   </body>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</html>
