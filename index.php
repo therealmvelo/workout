0628c00b9340eb928685ec2b648c439cd4c1e619
therealmvelo <2020130474@students,elangeni.edu.za>
2024-07-18 16:15:01 +0200

    cmt

--- index.php
+++ index.php
@@ -3,43 +3,41 @@
    <head>
        <meta charset="utf-8">
        <meta name="viewport" content="width=device-width, initial-scale=1">
-       <link rel="stylesheet" href="index.css">
+       <link rel="stylesheet" href="inndex.css">
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
        <title>workout</title>
    </head>
    <body>
-        <nav class="navbar navbar-expand-lg border border-success">
+        <nav class="navbar navbar-expand-lg border-bottom border-white sticky-top">
             <a href="#title" class="navbar-brand m-lg-2">Hello world</a>
             <ul class="navbar-nav justify-items-center m-auto d-flex">
-                <li class="nav-link">Home</li>
+                <li class="nav-link" href="#home">Home</li>
                 <li class="nav-link">About</li>
                 <li class="nav-link">Contact</li>
                 <li class="nav-link">Read more</li>
             </ul>
+            <h1>❤</h1>
         </nav>
         <div class="container">
             <div class="body-content">
-            <div id="imgsCarousel" class="carousel slide">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src="imgs/workout0.avif" class="d-block w-100" alt="...">
+
+                <div class="carousel m-auto mt-5 slide " id="imgCarousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="imgs/workout0.avif" alt="">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="imgs/workout1.avif" alt="">
+                        </div>
                     </div>
-                    <div class="carousel-item">
-                    <img src="imgs/workout0.avif" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                    <img src="imgs/workout0.avif" class="d-block w-100" alt="...">
-                    </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="imgCarousel" data-bs-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="imgCarousel" data-bs-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        </button>                    
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-                </div>
+
             </div>
         </div>
    </body>
