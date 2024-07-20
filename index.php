b4c4be8d645a13d9e85b5c085df39410ea71d7cc
therealmvelo <2020130474@students,elangeni.edu.za>
2024-07-20 18:57:18 +0200

    jk, mu

--- index.php
+++ index.php
@@ -4,55 +4,63 @@
        <meta charset="utf-8">
        <meta name="viewport" content="width=device-width, initial-scale=1">
        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-       <link rel="stylesheet" href="inndex.css">
+       <link rel="stylesheet" href="inndex.css?v=2">
        <title>workout</title>
+       <link rel="preconnect" href="https://fonts.googleapis.com">
+       <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+       <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
    </head>
-   <body>
-        <nav class="navbar navbar-expand-lg border-bottom border-white sticky-top navigation">
-            <a href="#title" class="navbar-brand m-lg-2">Hello world</a>
+   <body class="bg-dark">
+        <nav class="navbar navbar-expand-lg sticky-top navigation">
+            <a href="#title" class="navbar-brand m-lg-2"><h2>Author.x Smith</h2></a>
             <ul class="navbar-nav justify-items-center m-auto d-flex">
                 <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
+                    <a class="nav-link active h5" href="">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
+                    <a class="nav-link h5" href="">About</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link active" href="">Home</a>
+                    <a class="nav-link h5" href="">Books</a>
                 </li>
             </ul>
-            <h1>❤</h1>
         </nav>
-        <div class="container">
-            <div class="body-content">
 
-                <div id="myCarousel" class="carousel slide m-auto" >
-                    <div class="carousel-inner">
-                        <div class="carousel-item ">
-                            <img src="imgs/workout0.avif" class="d-block w-100" alt="">
-                        </div>
-                        <div class="carousel-item active">
-                            <img src="imgs/workout1.avif" class="d-block w-100" alt="">
+        <div class="container mt-5">
+            <div class="row">
+                <div class="col-6 ">
+                <ul class="author-intro text-white">
+                    <li class="author-intro-item">
+                        <h1>Author</h1>
+                        <div class="paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.</div>
+                        <div class="vertical-line"></div>
+                    </li>
+                    <li class="author-intro-item">
+                        <h1>Speaker</h1>
+                        
+                        <div class="paragraph">Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.</div>
+                        <div class="vertical-line"></div>
+                    </li>
+                    <li class="author-intro-item">
+                        <h1>Lecture</h1>
+                        <div class="paragraph">Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta.</div>
+                    </li>
+            </ul>
+                </div>
+                <div class="col-6">
+                    <div class="card text-danger author-card m-auto p-2">
+                        <img src="imgs/workout3.jpeg" class="card-img-top h-100" alt="...">
+                            <div class="card-img-overlay">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                            </div>
                         </div>
                     </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">previous</span>
-                        </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="visually-hidden">next</span>
-                        </button>                    
                 </div>
 
-            </div>
         </div>
+
    </body>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </html>
